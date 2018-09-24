7cb44cd0e05f42774fe6512c373fb7cd040b2dd3
ASUS <ASUS@DESKTOP-J1DNFS8>
2018-09-24 21:41:42 +0700

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Operasi = new System.Windows.Forms.Label();
+            this.NilaiA = new System.Windows.Forms.Label();
+            this.NilaiB = new System.Windows.Forms.Label();
+            this.Hasil = new System.Windows.Forms.Label();
+            this.operasiHitung = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.buttonhitung = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Operasi
+            // 
+            this.Operasi.AutoSize = true;
+            this.Operasi.Location = new System.Drawing.Point(12, 29);
+            this.Operasi.Name = "Operasi";
+            this.Operasi.Size = new System.Drawing.Size(43, 13);
+            this.Operasi.TabIndex = 0;
+            this.Operasi.Text = "Operasi";
+            this.Operasi.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // NilaiA
+            // 
+            this.NilaiA.AutoSize = true;
+            this.NilaiA.Location = new System.Drawing.Point(12, 58);
+            this.NilaiA.Name = "NilaiA";
+            this.NilaiA.Size = new System.Drawing.Size(37, 13);
+            this.NilaiA.TabIndex = 1;
+            this.NilaiA.Text = "Nilai A";
+            // 
+            // NilaiB
+            // 
+            this.NilaiB.AutoSize = true;
+            this.NilaiB.Location = new System.Drawing.Point(12, 97);
+            this.NilaiB.Name = "NilaiB";
+            this.NilaiB.Size = new System.Drawing.Size(37, 13);
+            this.NilaiB.TabIndex = 2;
+            this.NilaiB.Text = "Nilai B";
+            // 
+            // Hasil
+            // 
+            this.Hasil.AutoSize = true;
+            this.Hasil.Location = new System.Drawing.Point(12, 127);
+            this.Hasil.Name = "Hasil";
+            this.Hasil.Size = new System.Drawing.Size(30, 13);
+            this.Hasil.TabIndex = 3;
+            this.Hasil.Text = "Hasil";
+            this.Hasil.Click += new System.EventHandler(this.Hasil_Click);
+            // 
+            // operasiHitung
+            // 
+            this.operasiHitung.FormattingEnabled = true;
+            this.operasiHitung.Location = new System.Drawing.Point(80, 29);
+            this.operasiHitung.Name = "operasiHitung";
+            this.operasiHitung.Size = new System.Drawing.Size(149, 21);
+            this.operasiHitung.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(80, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(80, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(80, 58);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(149, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // buttonhitung
+            // 
+            this.buttonhitung.Location = new System.Drawing.Point(109, 174);
+            this.buttonhitung.Name = "buttonhitung";
+            this.buttonhitung.Size = new System.Drawing.Size(75, 23);
+            this.buttonhitung.TabIndex = 8;
+            this.buttonhitung.Text = "Hitung";
+            this.buttonhitung.UseVisualStyleBackColor = true;
+            this.buttonhitung.Click += new System.EventHandler(this.buttonhitung_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.buttonhitung);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.operasiHitung);
+            this.Controls.Add(this.Hasil);
+            this.Controls.Add(this.NilaiB);
+            this.Controls.Add(this.NilaiA);
+            this.Controls.Add(this.Operasi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Operasi;
+        private System.Windows.Forms.Label NilaiA;
+        private System.Windows.Forms.Label NilaiB;
+        private System.Windows.Forms.Label Hasil;
+        private System.Windows.Forms.ComboBox operasiHitung;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button buttonhitung;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            operasiHitung.Items.Add("Penambahan");
+            operasiHitung.Items.Add("Pengurangan");
+            operasiHitung.Items.Add("Perkalian");
+            operasiHitung.Items.Add("Pembagian");
+            operasiHitung.SelectedItem = ("Penambahan");
+        }
+
+        static int Penambahan(int a, int b)
+        {
+            return a+b;
+
+        }
+
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+
+        }
+        static int Perkalian(int a, int b)
+        {
+            return a * b;
+
+        }
+        static int Pembagian(int a, int b)
+        {
+            return a / b;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonhitung_Click(object sender, EventArgs e)
+        {
+            var a = Convert.ToInt32(textBox3.Text);
+            var b = Convert.ToInt32(textBox2.Text);
+            string ambilOperasi = Operasi.Text;
+
+            if (ambilOperasi == "Penambahan")
+            {
+                textBox1.Text = Convert.ToString(Penambahan(a, b));
+            }
+            else if (ambilOperasi == "Pengurangan")
+            {
+                textBox1.Text = Convert.ToString(Pengurangan(a, b));
+            }
+            else if (ambilOperasi == "Perkalian")
+            {
+
+                textBox1.Text = Convert.ToString(Perkalian(a, b));
+            }
+            else (ambilOperasi == "Pembagian")
+            {
+                textBox1.Text = Convert.ToString(Pembagian(a, b));
+            }
+        }
+
+            private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       private void Hasil_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
